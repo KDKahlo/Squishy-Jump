311d647ceeb98b8efdc5d5cd27f8c5a38e0c95f8
KD <kdkahlo@gmail.com>
2025-10-21 16:34:20 +0200

    Initial clean commit after adding gitignore

---
+++ Assets/SquishyScript.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class SquishyMove : MonoBehaviour
+{
+    //variable to interact with Rigidbody2D component
+    public Rigidbody2D SquishyRigidbody;
+    public float SquishyMoveSpeed;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) == true)        {
+            SquishyRigidbody.linearVelocity =  Vector2.up * 10;
+        }
+        
+    }
+}
